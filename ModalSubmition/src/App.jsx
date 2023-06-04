3dccd943961b76c4a0c83e3bee5b4ae17325f4fa
Usman Muneer <129029349+UsmanMuneer15@users.noreply.github.com>
2023-06-04 11:07:18 -0700

    Add files via upload
    add

---
+++ ModalSubmition/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import EditModal from './components/EditModal'
+import Contact from './components/Contact'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <EditModal/>
+     {/* <Contact/> */}
+     
+    </>
+  )
+}
+
+export default App
---
+++ ModalSubmition/src/components/Contact.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+function Contact() {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs
+      .sendForm(
+        'service_gcc664q', 
+        'template_6jsicy6', form.current, 
+        'Gvz6ehEKM20Rd5udR')
+        .then((result) => {
+            console.log(result.text);
+            console.log("message sent")
+        }, (error) => {
+            console.log(error.text);
+        });
+    };
+  return (
+    <div>
+
+<form ref={form} onSubmit={sendEmail}>
+    
+      <label>Name:</label><br></br>
+      <input  style={ { width: "30%" } } type="text" name="user_name" /><br></br>
+      <label>Email:</label><br></br>
+      <input  style={ { width: "30%" } } type="email" name="user_email" /><br></br>
+      <label>Phone No:</label><br></br>
+      <input style={ { width: "30%" } } type='country' name="user_phone"></input><br></br>
+      <label>Company Name:</label><br></br>
+      <input style={ { width: "30%" } } type='country' name="user_company"></input><br></br>
+      <label>Country:</label><br></br>
+      <input style={ { width: "30%" } } type='country' name="user_country"></input><br></br>
+      <label>State:</label><br></br>
+      <input style={ { width: "30%" } } type='country' name="user_state"></input><br></br>
+      <label>Message:</label><br></br>
+      <textarea   style={ { width: "30%" } }name="message" /><br></br>
+      <input type="submit" value="Send" /><br></br>
+    </form>
+    </div>
+  )
+}
+
+export default Contact
